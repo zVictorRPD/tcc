16058177bd076b2dd50703c155a3173dc8aa1d74
Victor Martins <victor2007azevedo@hotmail.com>
2022-09-07 00:16:24 -0300

    Adição de seed do prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+
+
+async function main() {
+    await prisma.user.upsert({
+        where: { email: "alice@prisma.io" },
+        update: {},
+        create: {
+            name: "Alice",
+            email: "admin@admin",
+            password: bcrypt.hashSync('12345678', 9),
+            avatar: "https://avatars.githubusercontent.com/u/746482?v=4",
+        },
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
