f47b143aea1e1c16cdaa5e848cb396e2156d01dd
Victor Martins <victor2007azevedo@hotmail.com>
2022-10-20 15:52:27 -0300

    Atualizando o banco
    Inseri as novas models que foram criadas com os dados do webScraping

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {
+    BASE_URL: process.env.BASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
     MAP_API_KEY: process.env.MAP_API_KEY,
   },
 }
