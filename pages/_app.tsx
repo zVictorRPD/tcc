132d9bd33e67c173443f2c7e62698b04c166e103
Victor Martins <victor2007azevedo@hotmail.com>
2022-11-17 19:23:56 -0300

    PWA
    O PWA permite que usuário baixe o site como se fosse um aplicativo, dessa forma fica muito mais fácil utilizar o mesmo.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import theme from "../theme";
 import LoggedContainer from "../src/components/Logged/Layout/LoggedContainer";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps, router }: AppProps) {   
+function MyApp({ Component, pageProps, router }: AppProps) {
     const url = router.pathname;
     const isLogged = url.split("/").includes("ambiente-logado");
-    
+
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <meta
+                    name='viewport'
+                    content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+                />
+            </Head>
             <SessionProvider session={pageProps.session}>
                 {isLogged ? (
                     <LoggedContainer pageName={url.split('ambiente-logado/')[1]}>
