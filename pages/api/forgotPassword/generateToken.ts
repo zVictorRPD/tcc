16eaf80b4491302a8d6a91071b978fbadb9ad46b
Victor Martins <victor2007azevedo@hotmail.com>
2022-12-04 10:44:04 -0300

    Email para recuperar senha

--- pages/api/forgotPassword/generateToken.ts
+++ pages/api/forgotPassword/generateToken.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { checkUserEmail, saveToken } from "../../../src/backend/users";
 import { sendEmailFunction } from "../../../src/backend/nodemailer";
 import bcrypt from "bcrypt";
+import { resetPasswordEmail } from "../../../src/components/Emails/resetPassword";
 
 export default async function handler(
     req: NextApiRequest,
@@ -10,24 +11,28 @@ export default async function handler(
 ) {
     if (req.method === "POST") {
         const { email } = req.body;
-        
+
         const checkEmail = await checkUserEmail(email);
 
         if (checkEmail?.code === 200) {
             //criar token aleatorio de 8 digitos
             const token = Math.floor(10000000 + Math.random() * 90000000).toString();
-            
+
             const encryptedToken = bcrypt.hashSync(token, 9);
             const tokenResponse = await saveToken(email, encryptedToken);
-            if(tokenResponse?.code === 200){
-                await sendEmailFunction([email], "Recuperação de senha", `<p>Seu token de recuperação de senha é: <strong>${token}</strong></p>`);               
+            if (tokenResponse?.code === 200) {
+                await sendEmailFunction(
+                    [email],
+                    "Recuperação de senha",
+                    resetPasswordEmail(token)
+                );
                 return res.status(200).json(tokenResponse);
             }
             return res.status(500).json({
                 code: 500,
                 message: "Token não foi salvo",
             });
-        } else if(checkEmail?.code === 404) {
+        } else if (checkEmail?.code === 404) {
             return res.status(200).json(checkEmail);
         }
     }
